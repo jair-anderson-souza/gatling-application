ef32fa71071f77b48f3ff30fac0075a2f4ac0a66
jass2125 <jair_anderson_bs@hotmail.com>
2021-05-30 19:18:51 -0300

    Deploy heroku

--- src/main/java/io/github/jass2125/gatling/service/PersonService.java
+++ src/main/java/io/github/jass2125/gatling/service/PersonService.java
@@ -4,6 +4,9 @@ import io.github.jass2125.gatling.model.Person;
 import io.github.jass2125.gatling.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -16,18 +19,22 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Cacheable(cacheNames = "persons")
     public List<Person> getAll(Pageable pageable) {
         return this.personRepository.findAll(pageable).getContent();
     }
 
+    @Cacheable(cacheNames = "persons", key = "#id")
     public Person getById(Long id) {
         return this.personRepository.findById(id).orElse(null);
     }
 
+    @CachePut(cacheNames = "persons", key = "#person.id")
     public Person update(Person person) {
         return this.personRepository.save(person);
     }
 
+    @CacheEvict(cacheNames = "persons", allEntries = true)
     public Person save(Person person) {
         return this.personRepository.save(person);
     }
